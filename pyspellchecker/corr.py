4cf1cddfded169975f3360e3b8f8cc542025e862
Ljudmila Petkovic <ljudmilapetkovic@MacBook-Pro-de-Ljudmila.local>
2021-07-07 15:16:44 +0200

    corrected the words initially ending with the punctuation sign (e.g. 'jeuneffe,')

--- pyspellchecker/corr.py
+++ pyspellchecker/corr.py
@@ -11,8 +11,10 @@ def listdir_nohidden(path):
     return glob.glob(os.path.join(path, '*'))
 
 
+
 # spécifier les docs d'entrée (échantillon) à partir desquels les sorties corrigées seront générées
 directory_in = listdir_nohidden("./sample_in/")
+
 
 
 # enlever l'extension .xml des fichiers d'entrée 
@@ -23,16 +25,19 @@ for file_in in directory_in:
     file_in = os.path.splitext(file_in)[0]
     # print(file_in) # 5419000_r, test
     
-    
+ 
+
     # créer les nouveaux fichiers .txt sur lesquels les corrections seront appliquées par la suite
     file_out = '{}'.format(file_in)+'.txt'
     # print(file_out) # 5419000_r.txt, test.txt
     directory_out = os.path.join("./sample_out/", file_out)
   
+
     
     # créer les nouveaux fichiers .csv où les erreurs avec leurs corrections seront enregistrées
     corr_out = os.path.join('./csv/', file_in+'.csv')
-    
+   
+
     
     # transférer le contenu des fichiers .xml dans les fichiers .txt
     # nettoyer les .txt
@@ -46,36 +51,45 @@ for file_in in directory_in:
                 if text: 
                     for c in car_spec:
                         text = text.replace(c,'')
-                    text = re.sub('« \n', '', text)
+                          
+                    
+                    # pré-traitements
+                    text = re.sub('« \n', '', text)
                     text = re.sub(" +", " ", text)
                     text = text.lower()
                     text = text.replace("\n", " ")
                     text = text.replace(' ,', ',')
                     text = text.replace("'", "’")
                     text = text.replace(' .','. ')
-                    text = text.replace(' :',':')
-                    text = text.replace(' ;',';')
-                    text = text.replace(' !','!')
-                    text = text.replace(' ?','?')
+                    text = text.replace(' :',':')
+                    text = text.replace(' ;',';')
+                    text = text.replace(' !','!')
+                    text = text.replace(' ?','?')
                     text = text.replace('" ','"')
                     text = text.replace('( ','(')
                     text = text.replace(' )',')') 
+                    
+                    
+                    
+                    # enlever les signes de ponctuation se trouvant à la fin d'un token
+                    # afin d'éviter que le correcteur corrige la séquence 'token + signe de ponctuation'
+                    # ex: 'jeuneffe,' (avec virgule) > 'jeuneffe' (incorrect)
+                    # au lieu de 'jeuneffe' (sans virgule) > 'jeunesse' (correct)
+                    text = re.sub('(?<=\w)[,;:?!.]', '', text)
+                    
                             
 
                     # définir le correcteur d'orthographe français 
                     # pyspellchecker
                     spell = SpellChecker(language='fr')
 
-                    
+                   
+                
                     # tokeniser le texte avec le tokeniseur standard (ex: 'l'empire')
                     # car celui de pyspellchecker tokenise mal (ex: 'l', 'empire')
                     token_list = text.split()
                     
 
-                    # tokeniser le texte avec le tokeniseur de pyspellchecker
-                    # tokens = spell.split_words(text)
-                    # print(content_list)                    
-                    # print(c)
 
                     # ne pas corriger les tokens contenant l'apostrophe (ex : l’empire, d’art, s’étend...)
                     r1 = re.findall(r"(l’\w+|l’\w+-\w+|d’\w+|d’\w+|qu’\w+|c’\w+|n’\w+|j’\w+|lorfqu’\w+|eft)",text)
@@ -83,13 +97,11 @@ for file_in in directory_in:
                     spell.known(r1)  # les mots {'ex : l’empire, d’art, s’étend'} sont désormais dans le dictionnaire des mots corrects
 
                     
+                    
                     # corriger les mots incorrects dans le fichier .txt
                     # extraire les erreurs, leurs fréquences et leurs corrections dans un tableur .csv
                     misspelled = spell.unknown(token_list)
                     for m in misspelled:
-                        if m in token_list:
-                            m_freq = token_list.count(m)
-                            # print(m_freq)
                         corrected = spell.correction(m)
                         text = text.replace(m, corrected)
                         # f.write(c.replace('clafliques', 'classiques'))
